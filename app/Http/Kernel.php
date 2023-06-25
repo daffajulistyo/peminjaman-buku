fb9d23a46ec9e5b945e4241a1abeba7eda3d5388
daffajulistyo <daffajulistyo88@gmail.com>
2023-06-25 13:12:06 +0700

    Add Middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         'api' => [
             // \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
@@ -64,5 +64,8 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'is_admin' => \App\Http\Middleware\IsAdmin::class,
+        'admin' => \App\Http\Middleware\AdminMiddleware::class,
+        'member' => \App\Http\Middleware\MemberMiddleware::class,
+
     ];
 }
--- routes/web.php
+++ routes/web.php
@@ -25,37 +25,39 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('book')->group(function () {
-    Route::get('list_book', [BookController::class, 'index'])->name('book.index');
-    Route::post('/insert_book', [BookController::class, 'store'])->name('book.insert');
-    Route::get('/add_book', [BookController::class, 'create'])->name('book.show');
-    Route::get('/edit_book/{id}', [BookController::class, 'edit'])->name('book.edit');
-    Route::post('/update_book/{id}', [BookController::class, 'update'])->name('book.update');
-    Route::get('/delete_book/{id}', [BookController::class, 'destroy'])->name('book.delete');
-});
-Route::prefix('user')->group(function () {
-    Route::get('list_user', [UserController::class, 'index'])->name('user.index');
-    Route::post('/insert_user', [UserController::class, 'store'])->name('user.insert');
-    Route::get('/add_user', [UserController::class, 'create'])->name('user.show');
-    Route::get('/edit_user/{id}', [UserController::class, 'edit'])->name('user.edit');
-    Route::post('/update_user/{id}', [UserController::class, 'update'])->name('user.update');
-    Route::get('/delete_user/{id}', [UserController::class, 'destroy'])->name('user.delete');
+Route::prefix('admin')->middleware('admin')->group(function () {
+    Route::prefix('book')->group(function () {
+        Route::get('list_book', [BookController::class, 'index'])->name('book.index');
+        Route::post('/insert_book', [BookController::class, 'store'])->name('book.insert');
+        Route::get('/add_book', [BookController::class, 'create'])->name('book.show');
+        Route::get('/edit_book/{id}', [BookController::class, 'edit'])->name('book.edit');
+        Route::post('/update_book/{id}', [BookController::class, 'update'])->name('book.update');
+        Route::get('/delete_book/{id}', [BookController::class, 'destroy'])->name('book.delete');
+    });
+    Route::prefix('user')->group(function () {
+        Route::get('list_user', [UserController::class, 'index'])->name('user.index');
+        Route::post('/insert_user', [UserController::class, 'store'])->name('user.insert');
+        Route::get('/add_user', [UserController::class, 'create'])->name('user.show');
+        Route::get('/edit_user/{id}', [UserController::class, 'edit'])->name('user.edit');
+        Route::post('/update_user/{id}', [UserController::class, 'update'])->name('user.update');
+        Route::get('/delete_user/{id}', [UserController::class, 'destroy'])->name('user.delete');
+    });
+    
+    Route::prefix('loan')->group(function () {
+        Route::get('list_loan', [LoanRequestController::class, 'index'])->name('loan.index');
+        Route::resource('loan-requests', LoanRequestController::class);
+        Route::put('loan-requests/{loan_request}/approve', [LoanRequestController::class, 'approve'])->name('loan-requests.approve');
+        Route::put('loan-requests/{loan_request}/reject', [LoanRequestController::class, 'reject'])->name('loan-requests.reject');
+        Route::post('list_request', [LoanRequestController::class, 'store'])->name('request.store');
+    });
+    
+    Route::prefix('loans')->group(function () {
+        Route::get('list', [LoanRequestController::class, 'list'])->name('list.loans.index');
+        Route::put('loans/{loan_request}/return', [LoanRequestController::class, 'returnBook'])->name('list.loans.return');
+    });
 });
 
-Route::prefix('loan')->group(function () {
-    Route::get('list_loan', [LoanRequestController::class, 'index'])->name('loan.index');
-    Route::resource('loan-requests', LoanRequestController::class);
-    Route::put('loan-requests/{loan_request}/approve', [LoanRequestController::class, 'approve'])->name('loan-requests.approve');
-    Route::put('loan-requests/{loan_request}/reject', [LoanRequestController::class, 'reject'])->name('loan-requests.reject');
-    Route::post('list_request', [LoanRequestController::class, 'store'])->name('request.store');
-});
-
-Route::prefix('admin')->group(function () {
-    Route::get('list', [LoanRequestController::class, 'list'])->name('list.loans.index');
-    Route::put('loans/{loan_request}/return', [LoanRequestController::class, 'returnBook'])->name('list.loans.return');
-});
-
-Route::prefix('member')->group(function () {
+Route::prefix('member')->middleware('member')->group(function () {
     Route::get('add-request', [LoanRequestController::class, 'create'])->name('request.index');
     Route::get('list-book', [LoanRequestController::class, 'listBookMember'])->name('member.book.index');
 });
